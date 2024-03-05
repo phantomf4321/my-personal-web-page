06c042f0913eee57ad0d56c7fb0a77432f3677c1
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2024-03-05 19:41:37 +0330

    about route

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/#about')
+@app.route('/index.html')
 def about():
-    return render_template('index.html#about')
+    return render_template('index.html')
 
 
 app.run(debug=True)
