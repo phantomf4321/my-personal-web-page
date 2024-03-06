3d58eb981c0f8c4525cc13fdacda1b7f08ab60e7
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2024-03-06 23:56:05 +0330

    profile image

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import *
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/about')
 def about():
     return redirect(url_for('index') + '#about')
