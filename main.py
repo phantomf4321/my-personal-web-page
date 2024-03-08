4c25a3968dbff598836460641a48c23495932810
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2024-03-08 20:15:40 +0330

    claculate_age on main

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    D1 = Defaults
-    about_data = [{"birthday": "25 May 2001", "birthdate": "2001/03/25", "age": D1.get_current_time()}]
+    D1 = Defaults()
+    about_data = [{"birthday": "25 May 2001", "birthdate": "2001/03/25", "age": D1.claculate_age()}]
     return render_template('index.html', data=about_data[0])
 
 @app.route('/about')
