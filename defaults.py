1abfbf630213fdc84bde16ea2bd987ed659a88d5
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2024-03-08 19:50:05 +0330

    test get_current_time

--- defaults.py
+++ defaults.py
@@ -2,8 +2,12 @@ from datetime import datetime
 
 class Defaults:
 
-    def get_current_time(self):
+    def get_current_time():
         date = datetime.now()
         date = date.strftime("%Y/%m/%d")
 
         return date
+
+
+D1 = Defaults
+print(D1.get_current_time())
