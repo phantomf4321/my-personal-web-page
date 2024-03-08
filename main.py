7abfd41289f34d30f7b95045a36d2df3cea2c29b
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2024-03-08 19:59:21 +0330

    about_data

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import *
 from defaults import Defaults
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-
-    return render_template('index.html')
+    D1 = Defaults
+    about_data = [{"birthday": "25 May 2001", "birthdate": "2001/03/25", "current_date": D1.get_current_time()}]
+    return render_template('index.html', data=about_data[0])
 
 @app.route('/about')
 def about():
